398639417a47593ff78fd8b20a76c3edf0c45043
emanuelklado <emanuelklado@msn.com>
2022-07-31 22:43:00 -0300

    commit pos avaliação

--- script.js
+++ script.js
@@ -3,24 +3,29 @@ const addToCart = document.getElementsByClassName('item__add');
 const clearCart = document.querySelector('.empty-cart');
 const itemLi = document.getElementsByClassName('cart__item');
 const listProducts = document.querySelector('.items');
-const loadMsg = document.createElement('p');
+const loadMsg = document.createElement('p');// criei fora para poder remover após carregamento.
 const pTotal = document.querySelector('.total-price');
+const sectionSelected = document.getElementsByClassName('selected');
+const input = document.getElementById('pesquisa');
+const iconCart = document.querySelector('.material-icons');
+const divCart = document.querySelector('.cart');
 
+// função de somar o carrinho de compras
 function sum() {
 const sumTotal = [];
 for (let index = 0; index < itemLi.length; index += 1) {
   const itemCartPrice = (Number(itemLi[index].innerText.split('$')[1]));
   sumTotal.push(itemCartPrice);
 }
- pTotal.innerText = sumTotal.reduce((acum, el) => acum + el, 0);
+ pTotal.innerText = `TOTAL R$ ${sumTotal.reduce((acum, el) => acum + el, 0).toFixed(2)}`;
 }
 
 function loading() {
-  loadMsg.innerText = 'carregando...';
+  loadMsg.innerText = '';
   loadMsg.className = 'loading';
   listProducts.appendChild(loadMsg);
 }
-
+// cria miniaturas;
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -34,15 +39,17 @@ function createCustomElement(element, className, innerText) {
   e.innerText = innerText;
   return e;
 }
-
-function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
+// add os produtos na pagina. (todas as sections)
+function createProductItemElement({ id: sku, title: name, thumbnail: image, price }) {
   const section = document.createElement('section');
+  const imageHD = image.replace('-I.jpg', '-J.jpg'); // traz imagem com qualidade superior. dica na monitoria de Daniel.
   section.className = 'item';
+  const priceFormated = `R$ ${price.toFixed(2)}`;
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
+  section.appendChild(createProductImageElement(imageHD));
+  section.appendChild(createCustomElement('span', 'price-product', priceFormated));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-  
   return section;
 }
 
@@ -52,7 +59,8 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
 
 // exclui as linhas e salva
 function cartItemClickListener(event) {
-  event.target.remove();
+  // event.target.remove();
+  event.currentTarget.remove(); // pra pegar o pai (section) e deletar imagem junto.
   saveCartItems(listCart.innerHTML);
   sum();
 }
@@ -63,13 +71,20 @@ clearCart.addEventListener('click', () => {
   sum();
   });
 
-function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+// cria a lista do carrinho.
+function createCartItemElement({ thumbnail: img, id: sku, title: name, price: salePrice }) {
+  const section = document.createElement('section');
+  section.className = 'selected';
   const li = document.createElement('li');
+  const imgMini = document.createElement('img');
+  imgMini.src = img;
+  section.appendChild(imgMini);
   li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  li.innerText = `COD: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  section.appendChild(li);
+  section.addEventListener('click', cartItemClickListener);
 
-  return li;
+  return section;
 }
 
 // cria as linhas, renderiza os itens e salva.
@@ -83,7 +98,7 @@ sum();
 // permite remover itens apos atualizar pagina
 function removeLinesAfterLoading() {
 for (let index = 0; index < itemLi.length; index += 1) {
-  itemLi[index].addEventListener('click', cartItemClickListener);
+  sectionSelected[index].addEventListener('click', cartItemClickListener);
 }
 }
 // percorre os elementos e adiciona evento click em todos eles.(botões);
@@ -92,19 +107,36 @@ function catchID() {
    addToCart[index].addEventListener('click', getIdElement); 
   }
 }
+// função generica recebendo o parametro de busca dos produtos.
+async function renderProducts(product) {
+  const products = await fetchProducts(product);
+  products.forEach((element) => {
+  listProducts.appendChild(createProductItemElement(element));
+});
+  catchID();
+}
+// evento do input para buscar novos produtos na API.
+input.addEventListener('change', () => {
+listProducts.innerHTML = '';
+renderProducts(input.value);
+});
+
+function showCart() {
+  if (divCart.style.display === 'none' || divCart.style.display === '') {
+    divCart.style.display = 'flex';
+  } else {
+    divCart.style.display = 'none';
+  }
+}
+iconCart.addEventListener('click', showCart);
 
 window.onload = async () => {
   loading();
-  // passando o obj retornado da função fetchProducts
-  const products = await fetchProducts('computador');
-  // passando cada elemento do obj como paramentro da função que cria os produtos.
-  products.forEach((element) => {
-    listProducts.appendChild(createProductItemElement(element));
-  });
-  loadMsg.remove();
-  catchID();
-  getSavedCartItems();
-  listCart.innerHTML = getSavedCartItems();
-  removeLinesAfterLoading();
-  sum();
+  setTimeout(() => {
+    loadMsg.remove();
+    renderProducts('computador');
+    listCart.innerHTML = getSavedCartItems();
+    removeLinesAfterLoading();
+    sum();
+  }, 1500);
 };
